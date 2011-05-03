1637b430a02bfc8670840a84b8a8be3f067cb04f
Aziz M. Bookwala <aziz@mantik.in>
2011-05-03 15:14:59 +0530

    Basic implementation using actors

--- assignment1/app/controllers.scala
+++ assignment1/app/controllers.scala
@@ -11,39 +11,18 @@ import play.data.validation._
 
 object Application extends Controller {
         
-    def processURL(@Required url:String) = {
+	val urlActor = new UrlParseActor
+	urlActor.start
+
+	def processURL(@Required url:String) = {
 		if(validation.hasErrors) {
 			flash += "error" -> "You left the URL field blank!"
 		} else {
-			while (Cache.get("url")!=None){
-				//print ("limbo")
-			}
-						
-			var results = List[List[String]]()
-			if (url != null) {
-				try {
- 					var devoded_url = URLDecoder.decode(url, "utf-8")
-					var html = parse(devoded_url)
-					List("h1", "h2", "h3", "h4", "h5", "h6").foreach {
-					(heading)=>
-						html \\ heading foreach { 
-								(n) =>
-									var i = List(heading, (n).text)
-									results ::= i		
-						}
-					}
-					Cache.add("url", url, "5s")
-					results = results.filter(h => countVowels(h(1))>=4).sort((e1, e2) => (countVowels(e1(1)) > countVowels(e2(1))))
-					Cache.set("report", results)
-					Cache.set("report_url", devoded_url)
-				} catch {
-					case e: org.xml.sax.SAXParseException => flash.error("There was an error in parsing the page. Probably malformed HTML");
-					case e => flash.error("Error: "+e.getMessage);
-				}
-			}
+			var results = (urlActor !? url)
+			print (results)
 		}
 		Action(index)
-    }
+	}
     
 	def index() = {
 		var results = Cache.get("report")
@@ -55,23 +34,50 @@ object Application extends Controller {
 		}
 		
 	}
-		
-	def countVowels(text:String):Int = {
-			var count = 0
-			text.toLowerCase().foreach { c=> 
-				if (c=='a' || c=='e' || c=='i' || c=='o' || c=='u') {
-							count+=1
-				}
-			}
-			return count
-		}
-	
-	def parse(sUrl:String):Elem = {
-			import java.net._
-			
-			var url = new URL(sUrl)
-			var connect = url.openConnection
-			XML.load(connect.getInputStream)
-		}
     
 }
+
+import scala.actors._
+class UrlParseActor extends Actor { 
+	def act = {
+		loop {
+			react {  // Like receive, but uses thread polling for efficiency.
+				case url: String => 
+					print (url)
+					var results = List[List[String]]()
+					var devoded_url = URLDecoder.decode(url, "utf-8")
+					var html = parse(devoded_url)
+					List("h1", "h2", "h3", "h4", "h5", "h6").foreach {
+						(heading)=>
+							html \\ heading foreach { 
+								(n) =>
+									var i = List(heading, (n).text)
+									results ::= i		
+						}
+					}
+					results = results.filter(h => countVowels(h(1))>=4).sort((e1, e2) => (countVowels(e1(1)) > countVowels(e2(1))))
+					reply(results)
+				
+			}
+		}
+	}
+
+	def parse(sUrl:String):Elem = {
+		import java.net._
+	
+		var url = new URL(sUrl)
+		var connect = url.openConnection
+		XML.load(connect.getInputStream)
+	}
+
+	def countVowels(text:String):Int = {
+		var count = 0
+		text.toLowerCase().foreach { c=> 
+			if (c=='a' || c=='e' || c=='i' || c=='o' || c=='u') {
+						count+=1
+			}
+		}
+		return count
+	}
+} 
+
